bb70bebbda90b83fb8e8d512a7489523933c1e68
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-06 23:05:50 +0200

    push exercice_01

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeBorder(image){
+    document.getElementById(image).style.border="22px solid black";
+}
+// function changeBorder(image){
+//     document.getElementById(image).style.boxShadow = "10px 20px 30px blue";
+// }
