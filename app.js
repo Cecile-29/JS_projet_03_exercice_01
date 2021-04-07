d2d125e36681056b1e52bb6182eb85d6b131c3ca
Cecile-29 <cecile.auger.pro@gmail.com>
2021-04-07 21:04:22 +0200

    push exercice après correction et commentaires

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-function changeBorder(image){
-    document.getElementById(image).style.border="22px solid black";
+function changeBorder(image) {
+	document.getElementById(image).style.border = '2px solid black';
 }
+
+function hideBorder(image) {
+	document.getElementById(image).style.border = 'none';
+}
+// idée de css différent
 // function changeBorder(image){
 //     document.getElementById(image).style.boxShadow = "10px 20px 30px blue";
 // }
